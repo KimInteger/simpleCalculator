d5177a9a076771fc211fdbe5b6e8ccf54d41970a
김정수 <kjs20151240@gmail.com>
2024-08-26 21:01:49 +0900

    plate | app.tsx
    전체적인 틀 선언만 해두었다.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const clickNumPad = () => {};
+
+  const clickSignPad = () => {};
+
+  const getResult = (prevInput: string, Input: string): void => {
+    setInputValue();
+  };
   return (
-    <>
-      <h1>계산기 만들기</h1>
-    </>
+    <div>
+      <div>
+        <input type="text" id="dashboard" value={inputValue} readOnly />
+      </div>
+      <div>
+        <button onClick={}>1</button>
+        <button onClick={}>2</button>
+        <button onClick={}>3</button>
+        <button onClick={}>+</button>
+      </div>
+      <div>
+        <button onClick={}>4</button>
+        <button onClick={}>5</button>
+        <button onClick={}>6</button>
+        <button onClick={}>-</button>
+      </div>
+      <div>
+        <button onClick={}>7</button>
+        <button onClick={}>8</button>
+        <button onClick={}>9</button>
+        <button onClick={}>*</button>
+      </div>
+      <div>
+        <button onClick={}>C</button>
+        <button onClick={}>0</button>
+        <button onClick={}>/</button>
+        <button onClick={}>=</button>
+      </div>
+    </div>
   );
 };
 
