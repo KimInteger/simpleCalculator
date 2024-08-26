d776a4ffcb6b1abf448da365ea62e91f646de34a
김정수 <kjs20151240@gmail.com>
2024-08-26 20:33:14 +0900

    wrench | webpack.config.js
    modules를 작성하지 않고, rules를 작성
    
    module기입하고 rules를 안에 기입

--- webpack.config.js
+++ webpack.config.js
@@ -43,22 +43,24 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: 'babel-loader',
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.(ts|tsx)$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    },
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     extensions: ['.css', '.js', '.jsx', '.ts', '.tsx'],
   },
