57acab84f9bb7ba6758ee8443af31a8778e487c1
김정수 <kjs20151240@gmail.com>
2024-08-27 12:15:33 +0900

    feat | app.tsx
    operator 연산자 관련 매서드 기능 추가

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,9 @@ const App: React.FC = () => {
     setInputValue(inputValue + numValue);
   };
 
-  const clickSignPad = (selectOP: string): void => {
+  const clickSignPad = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    let opPad = e.currentTarget;
+    let selectOP: string = opPad.textContent || '';
     setPrevInputValue(inputValue);
     setOperator(selectOP);
     setInputValue('');
@@ -39,24 +41,24 @@ const App: React.FC = () => {
         <button onClick={clickNumPad}>1</button>
         <button onClick={clickNumPad}>2</button>
         <button onClick={clickNumPad}>3</button>
-        <button onClick={}>+</button>
+        <button onClick={clickSignPad}>+</button>
       </div>
       <div>
         <button onClick={clickNumPad}>4</button>
         <button onClick={clickNumPad}>5</button>
         <button onClick={clickNumPad}>6</button>
-        <button onClick={}>-</button>
+        <button onClick={clickSignPad}>-</button>
       </div>
       <div>
         <button onClick={clickNumPad}>7</button>
         <button onClick={clickNumPad}>8</button>
         <button onClick={clickNumPad}>9</button>
-        <button onClick={}>*</button>
+        <button onClick={clickSignPad}>*</button>
       </div>
       <div>
         <button onClick={clearDashboard}>C</button>
         <button onClick={clickNumPad}>0</button>
-        <button onClick={}>/</button>
+        <button onClick={clickSignPad}>/</button>
         <button onClick={}>=</button>
       </div>
     </div>
