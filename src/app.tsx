b6a97d46c5e53b14df6714bcead8effe2edcc0a7
김정수 <kjs20151240@gmail.com>
2024-08-27 12:07:22 +0900

    feat | app.tsx
    넘버패드 클릭시 해당 button의 textContent를 inputValue에 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 
 const App: React.FC = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState<string>('');
+  const [prevInputValue, setPrevInputValue] = useState<string>('');
+  const [operator, setOperator] = useState<string>('');
 
-  const clickNumPad = () => {};
+  const clickNumPad = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    let numBtn = e.currentTarget;
+    let numValue = numBtn.textContent;
+    setInputValue(inputValue + numValue);
+  };
 
-  const clickSignPad = () => {};
+  const clickSignPad = (selectOP: string): void => {
+    setPrevInputValue(inputValue);
+    setOperator(selectOP);
+    setInputValue('');
+  };
 
-  const getResult = (prevInput: string, Input: string): void => {
+  const getResult = (
+    prevInput: string,
+    Input: string,
+    operator: string,
+  ): void => {
+    let result: string = '';
+
     setInputValue();
+  };
+
+  const clearDashboard = (): void => {
+    setInputValue(''), setOperator(''), setPrevInputValue('');
   };
   return (
     <div>
@@ -16,26 +36,26 @@ const App: React.FC = () => {
         <input type="text" id="dashboard" value={inputValue} readOnly />
       </div>
       <div>
-        <button onClick={}>1</button>
-        <button onClick={}>2</button>
-        <button onClick={}>3</button>
+        <button onClick={clickNumPad}>1</button>
+        <button onClick={clickNumPad}>2</button>
+        <button onClick={clickNumPad}>3</button>
         <button onClick={}>+</button>
       </div>
       <div>
-        <button onClick={}>4</button>
-        <button onClick={}>5</button>
-        <button onClick={}>6</button>
+        <button onClick={clickNumPad}>4</button>
+        <button onClick={clickNumPad}>5</button>
+        <button onClick={clickNumPad}>6</button>
         <button onClick={}>-</button>
       </div>
       <div>
-        <button onClick={}>7</button>
-        <button onClick={}>8</button>
-        <button onClick={}>9</button>
+        <button onClick={clickNumPad}>7</button>
+        <button onClick={clickNumPad}>8</button>
+        <button onClick={clickNumPad}>9</button>
         <button onClick={}>*</button>
       </div>
       <div>
-        <button onClick={}>C</button>
-        <button onClick={}>0</button>
+        <button onClick={clearDashboard}>C</button>
+        <button onClick={clickNumPad}>0</button>
         <button onClick={}>/</button>
         <button onClick={}>=</button>
       </div>
