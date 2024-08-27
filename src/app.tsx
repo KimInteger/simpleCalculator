6e98ca1aaeb7981fcdb9f21c2c8f23e2c41b7834
김정수 <kjs20151240@gmail.com>
2024-08-27 12:35:54 +0900

    anno | app.tsx
    주석 작성

--- src/app.tsx
+++ src/app.tsx
@@ -5,12 +5,21 @@ const App: React.FC = () => {
   const [prevInputValue, setPrevInputValue] = useState<string>('');
   const [operator, setOperator] = useState<string>('');
 
+  /**
+   * @brief 넘버 패드를 클릭하면 그 값이 DashBoard에 추가되는 기능
+   * @param e 해당 요소의 textContent
+   */
   const clickNumPad = (e: React.MouseEvent<HTMLButtonElement>): void => {
     let numBtn = e.currentTarget;
     let numValue = numBtn.textContent;
     setInputValue(inputValue + numValue);
   };
 
+  /**
+   * @brief 연산자 버튼을 set하는 기능
+   * @details 입력되어 있는 inputValue값을 prevInput에 Set하고 연산자를 Set함. 이후 DashBoard 클린
+   * @param e 해당 요소의 textContent를 감지하기 위함.
+   */
   const clickSignPad = (e: React.MouseEvent<HTMLButtonElement>): void => {
     let opPad = e.currentTarget;
     let selectOP: string = opPad.textContent || '';
@@ -19,6 +28,10 @@ const App: React.FC = () => {
     setInputValue('');
   };
 
+  /**
+   * @brief 2개의 숫자를 연산하는 기능
+   * @details Set되어있는 2개의 값을 Set되어있는 연산자에 따라 연산한 이후 DashBoard에 값을 나타냄.
+   */
   const getResult = (): void => {
     let prevValue: number = parseInt(prevInputValue, 10);
     let currenValue: number = parseInt(inputValue, 10);
@@ -46,6 +59,9 @@ const App: React.FC = () => {
     }
   };
 
+  /**
+   * @brief DashBoard를 비롯한 모든 State를 공란으로 할당하는 기능
+   */
   const clearDashboard = (): void => {
     setInputValue(''), setOperator(''), setPrevInputValue('');
   };
